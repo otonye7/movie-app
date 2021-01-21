5f98c4dcea91092e1b128d63474bba44ed0961ec
otonye <otonyeamie@gmail.com>
2021-01-21 13:49:57 +0100

    Amaster

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Row from './row';
 import requests from './request';
+import Banner from './banner';
 
 function App() {
   return (
-    <div>
-     <h1>Application</h1>
-     <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}/>
+    <div className="app">
+     <Banner />
+     <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
+     <Row title="TOP RATED" fetchUrl={requests.fetchTopRated}/>
+     <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies}/>
+     <Row title="COMEDY MOVIES" fetchUrl={requests.fetchCommedyMovies}/>
+     <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies}/>
+     <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies}/>
+     <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/row.js
+++ src/row.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
+import './row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({title, fetchUrl}) => {
+const Row = ({title, fetchUrl, isLargeRow}) => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -20,8 +21,9 @@ const Row = ({title, fetchUrl}) => {
      <div className='row'>
         <h2>{title}</h2>
         <div className='row_posters'>
-            {movies.map(movie => (
-                <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+            {movies.map((movie) => (
+                <img key={movie.id} className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+                 src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
             ))}
         </div>
     </div>   
